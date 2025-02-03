49459963b4ce205f5333d07d992faec48e00d786
pchchv <ipchchv@gmail.com>
2025-02-03 10:11:24 +0300

    Add command-line argument parsing using clap

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,40 @@
+use std::path::PathBuf;
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// config file path. (Accept: .json, .toml)
+    #[clap(short='f', long)]
+    config: Option<PathBuf>,
+
+    /// static files folder path.
+    #[clap()]
+    assets: Option<PathBuf>, 
+
+    /// port number to run the server on.
+    #[clap(short, long)]
+    port: Option<u16>,
+
+    /// public key file path.
+    #[clap(short, long)]
+    cert: Option<PathBuf>,
+    
+    /// private key file path.
+    #[clap(short, long)]
+    key: Option<PathBuf>,
+
+    /// allow CORS from all origins.
+    #[clap(short='o', long)]
+    allow_cors: bool,
+
+    /// all files, include hidden files
+    #[clap(short, long)]
+    all: bool,
+
+    /// ignore files based on glob match
+    #[clap(short, long)]
+    ignore: Option<String>,
+}
+
 fn main() {}
