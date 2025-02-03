2d4b2bf43a44ae7ebb6a5f3f4b7c57333163a3e2
pchchv <ipchchv@gmail.com>
2025-02-03 10:42:21 +0300

    Add debug function for logging HTTP request details

--- src/debug.rs
+++ src/debug.rs
@@ -1,5 +1,22 @@
 use chrono::{Local, DateTime};
 
-pub fn time_string (time: DateTime<Local>) -> String {
+pub fn time_string(time: DateTime<Local>) -> String {
     time.format("%Y-%m-%d %H:%M:%S").to_string()
+}
+
+pub fn debug(method: &str,path: &str,status: Option<u16>,error: &str) -> () {
+    println!("[{}] {} {} {}{}", 
+        time_string(Local::now()),
+        method,
+        path,
+        match status {
+            Some(status) => status.to_string(),
+            None => String::from("...")
+        },
+        if error.len() > 0 {
+            format!("\n{}", error)
+        } else {
+            String::new()
+        }
+    );
 }
