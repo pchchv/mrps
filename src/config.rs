48f119f6c51ba489e4f4347de23a34fc23a573ca
pchchv <ipchchv@gmail.com>
2025-02-03 10:21:22 +0300

    Add Config struct for application configuration management

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,4 @@
+use std::path::PathBuf;
 use serde_derive::Deserialize;
 
 #[derive(Deserialize, Clone, Debug)]
@@ -5,4 +6,18 @@ pub struct Route {
     pub path: String,
     pub method: String,
     pub template: String
+}
+
+#[derive(Deserialize, Clone, Debug, Default)]
+pub struct Config {
+    pub all: Option<bool>,
+    pub key: Option<PathBuf>,
+    pub cors: Option<Vec<String>>,
+    pub port: Option<u16>,
+    pub cert: Option<PathBuf>,
+    pub data: Option<PathBuf>, 
+    pub ignore: Option<Vec<String>>,
+    pub assets: Option<PathBuf>, 
+    pub routes: Option<Vec<Route>>,
+    pub templates: Option<PathBuf>
 }
