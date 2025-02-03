5da94628b4abc1e050e972b342381e96c96237a5
pchchv <ipchchv@gmail.com>
2025-02-03 10:43:34 +0300

    Implement getter method for Assets struct to handle file retrieval and response generation

--- src/assets.rs
+++ src/assets.rs
@@ -9,7 +9,7 @@ pub struct Assets {
 }
 
 impl Assets {
-    pub fn new (all: bool, dir: PathBuf, ignore: Vec<String>) -> Result<Assets, Box<dyn Error>> {
+    pub fn new(all: bool, dir: PathBuf, ignore: Vec<String>) -> Result<Assets, Box<dyn Error>> {
         let p = dir.as_path();
         if !p.is_dir() {
             Err(format!("assets is not a dir: {}", p.display()).into())
@@ -20,5 +20,60 @@ impl Assets {
                 ignore
             })
         }
+    }
+
+    fn getter(&self, path_str: &str) -> Result<Response, StatusCode> {
+        let path = Path::new(path_str);
+        let dir = self.dir.as_path();
+        let mut file = dir.join(path);
+        if file.is_dir() {
+            file = file.join("index.html");
+        }
+
+        if !file.starts_with(dir) || !file.is_file() {
+            return Err(StatusCode::NOT_FOUND);
+        }
+
+        let path_str = path.to_str().unwrap_or("");
+        for glob in &self.ignore {
+            if glob_match(&glob, path_str) {
+                return Err(StatusCode::NOT_FOUND);
+            }
+        }
+
+        if !self.all {
+            for component in path.components() {
+                let name = component.as_os_str().to_str().unwrap_or("");
+                
+                if name.len() == 0 || (
+                    name.len() > 1 &&
+                    name.as_bytes()[0] == b'.'
+                ) {
+                    return Err(StatusCode::NOT_FOUND);
+                } 
+            }
+        }
+        
+        let mut response: Response;
+        match read(&file) {
+            Err(_) => {
+                return Err(StatusCode::INTERNAL_SERVER_ERROR);
+            },
+            Ok(body) => {
+                response = Response::new(body.into());
+            }
+        };
+
+        let mime = mime_guess::from_path(&file).first_raw().unwrap_or("");
+        if mime.len() > 0 {
+            match HeaderValue::from_str(mime) {
+                Ok(mime) => {
+                    response.headers_mut().insert(CONTENT_TYPE, mime);
+                },
+                Err(_) => {}
+            };
+        }
+
+        Ok(response)
     }
 }
