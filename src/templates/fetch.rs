cc8ea3648c58908eb63a8e84bc2da1e855ba8c95
pchchv <ipchchv@gmail.com>
2025-02-04 09:48:48 +0300

    Add new method to Res struct for creating instances from reqwest Response

--- src/templates/fetch.rs
+++ src/templates/fetch.rs
@@ -1,5 +1,7 @@
+use minijinja::Value;
 use serde_derive::Serialize;
 use std::collections::HashMap;
+use reqwest::blocking::Response;
 
 #[derive(Serialize)]
 struct Res {
@@ -9,4 +11,28 @@ struct Res {
 }
 
 impl Res {
+    fn new(response: Response) -> Value {
+        let status = response.status().as_u16();
+        let mut headers = HashMap::new();
+        for key in response.headers().keys() {
+            if let Some(value) = response.headers().get(key) {
+                if let Ok(value) = value.to_str() {
+                    headers.insert(key.to_string(), value.to_string());
+                }
+            }
+        }
+
+        match response.bytes() {
+            Ok(body) => Value::from_serialize(Res {
+                status,
+                body: body.to_vec(),
+                headers
+            }),
+            Err(_) => Value::from_serialize(Res {
+                status,
+                body: Vec::new(),
+                headers
+            })
+        }
+    }
 }
