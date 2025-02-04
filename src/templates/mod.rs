ebd3dd70f875d2b0e63fb81227af1fe817b44ac1
pchchv <ipchchv@gmail.com>
2025-02-04 09:37:15 +0300

    Add filters and functions to template module for enhanced data processing

--- src/templates/mod.rs
+++ src/templates/mod.rs
@@ -1,13 +1,18 @@
 mod parse;
 mod format;
 
+use parse::parse;
 use std::error::Error;
 use std::path::PathBuf;
+use format::{format, bytes};
 use minijinja::{Environment, path_loader};
 
 pub fn new(dir: PathBuf) -> Result<Environment<'static>, Box<dyn Error>> {
     let mut env = Environment::new();
     env.set_loader(path_loader(dir));
+    env.add_filter("parse", parse);
+    env.add_filter("bytes", bytes);
+    env.add_filter("format", format);
     env.add_function("log", |message: &str| -> () {
         println!("{}", message);
         ()
