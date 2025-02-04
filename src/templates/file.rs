505dc4a89fe267e1bc64872112c7dd841501c4fa
pchchv <ipchchv@gmail.com>
2025-02-04 10:17:26 +0300

    Add path validation method to IO struct

--- src/templates/file.rs
+++ src/templates/file.rs
@@ -1,5 +1,5 @@
 use std::error::Error;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use serde_derive::Serialize;
 
 #[derive(Serialize)]
@@ -20,12 +20,27 @@ pub struct IO {
 }
 
 impl IO {
-    pub fn new (dir: PathBuf) -> Result<IO, Box<dyn Error>> {
+    pub fn new(dir: PathBuf) -> Result<IO, Box<dyn Error>> {
         let p = dir.as_path();
         if p.is_dir() {
             Ok(IO {dir})
         } else {
             Err(format!("Data must be a directory: {}", p.display()).into())
+        }
+    }
+
+    fn get_path(&self, path: &str) -> Option<PathBuf> {
+        let dir = self.dir.as_path();
+        let mut path = Path::new(path);
+        if let Ok(p) = path.strip_prefix("/") {
+            path = p
+        }
+
+        let path = dir.join(path);
+        if path.starts_with(dir) {
+            Some(path)
+        } else {
+            None
         }
     }
 } 
