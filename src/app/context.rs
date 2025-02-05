62d88055edaee41e6f3588699b73de5f60e0d0c3
pchchv <ipchchv@gmail.com>
2025-02-05 10:23:53 +0300

    Add new constructor to Context for initializing with request data and headers

--- src/app/context.rs
+++ src/app/context.rs
@@ -1,5 +1,8 @@
+use axum::body::Bytes;
 use serde_derive::Serialize;
 use std::collections::HashMap;
+use axum::extract::MatchedPath;
+use axum::http::{Uri, HeaderMap, Method};
 
 #[derive(Serialize)]
 pub struct Context {
@@ -15,4 +18,32 @@ pub struct Context {
 }
 
 impl Context {
+    pub fn new (
+        route: MatchedPath,
+        params: HashMap<String, String>,
+        vars: HashMap<String, String>,
+        method: Method,
+        url: Uri,
+        raw_headers: HeaderMap,
+        body: Bytes 
+    ) -> Context {
+        let mut headers: HashMap<String, String> = HashMap::new();
+        for (key, value) in raw_headers.iter() {
+            if let Ok(value) = value.to_str() {
+                headers.insert(key.to_string(), value.to_string());
+            }
+        }
+        
+        Context {
+            method: method.as_str().to_string(),
+            url: url.to_string(),
+            route: route.as_str().to_string(),
+            path: url.path().to_string(),
+            query: url.query().unwrap_or("").to_string(),
+            params,
+            vars,
+            headers,
+            body: body.to_vec()
+        }
+    }
 }
