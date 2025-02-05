f70ec6076dbf0bda2da60383022efca1b98f05cd
pchchv <ipchchv@gmail.com>
2025-02-05 10:19:09 +0300

    Add AppState struct to manage application state with environment and template

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -1,3 +1,14 @@
 use minijinja::Environment;
+use axum::http::HeaderValue;
 
 type Env = Environment<'static>;
+
+#[derive(Clone)]
+pub struct AppState {
+    env: Env,
+    template: String,
+    mime: Option<HeaderValue>
+}
+
+impl AppState {
+}
