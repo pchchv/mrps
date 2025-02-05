d2ab3bc92eb1d331fe29e19ef3bf961b76d55058
pchchv <ipchchv@gmail.com>
2025-02-05 10:07:14 +0300

    Add command execution function with output serialization

--- src/templates/command.rs
+++ src/templates/command.rs
@@ -1,3 +1,5 @@
+use minijinja::Value;
+use std::process::Command;
 use serde_derive::Serialize;
 
 #[derive(Serialize)]
@@ -5,4 +7,25 @@ struct Output {
     code: i32,
     stdout: Vec<u8>,
     stderr: Vec<u8>
+}
+
+pub fn command(command: String) -> Value {
+    let result = match Command::new("sh").arg("-c").arg(&command).output() {
+        Ok(result) => result,
+        Err(err) => {
+            return Value::from_serialize(Output {
+                code: 999999,
+                stdout: format!(
+                    "Fail to execute command!"
+                ).as_bytes().to_vec(),
+                stderr: err.to_string().as_bytes().to_vec()
+            });
+        }
+    };
+
+    Value::from_serialize(Output {
+        code: result.status.code().unwrap_or(0),
+        stdout: result.stdout,
+        stderr: result.stderr
+    })
 }
