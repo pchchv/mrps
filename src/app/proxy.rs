0d6b947100d66017b0f009b348550d99dd163f63
pchchv <ipchchv@gmail.com>
2025-02-05 10:30:40 +0300

    Add async constructor to Proxy for handling requests with dynamic method, headers, and body

--- src/app/proxy.rs
+++ src/app/proxy.rs
@@ -1,5 +1,12 @@
+use minijinja::Value;
+use axum::body::Body;
+use std::error::Error;
+use serde::Deserialize;
+use crate::debug::debug;
 use std::collections::HashMap;
 use serde_derive::Deserialize;
+use axum::http::{StatusCode, HeaderMap};
+use reqwest::{Request, RequestBuilder, Client};
 
 #[derive(Deserialize)]
 pub struct Proxy {
@@ -10,4 +17,45 @@ pub struct Proxy {
 }
 
 impl Proxy {
+    pub async fn new (method: &str, headers: &HashMap<String, String>, body: &Vec<u8>,proxy: &Value) -> Result<(StatusCode, HeaderMap, Body), Box<dyn Error>> {
+        let proxy = Proxy::deserialize(proxy)?;
+        let method = proxy.method.unwrap_or(method.to_string());
+        debug(&method, &proxy.url, None, "");
+        let mut r = RequestBuilder::from_parts(Client::new(),
+            Request::new(method.parse()?, proxy.url.parse()?)
+        );
+        if let Some(headers) = proxy.headers {
+            for (name, value) in headers.iter() {
+                r = r.header(name, value);
+            }
+        }
+
+        for (name, value) in headers.iter() {
+            r = r.header(name.clone(), value.clone());
+        }
+        
+        let response = match r.body(
+            proxy.body.unwrap_or(body.to_vec())
+        ).send().await {
+            Ok(response) => {
+                debug(
+                    &method,
+                    &proxy.url,
+                    Some(response.status().as_u16()),
+                    ""
+                );
+                response
+            },
+            Err(err) => {
+                debug(&method, &proxy.url, Some(500), &err.to_string());
+                return Err(err.into());
+            }
+        };
+
+        Ok((
+            response.status(),
+            response.headers().clone(),
+            response.bytes().await?.into()
+        ))
+    }
 }
